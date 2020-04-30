abd0b97cb18e21a36b73bd46da94b1b9931d8c72
gokulmaxi <gokulakams@gmail.com>
2020-05-01 02:58:37 +0530

    error corrected

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-
+  currentNumber = '0';
+  fistOperand = null;
+  operator = null;
+  waitForSecondNumber =false;
+  public getNumber(v: string){
+    console.log(v);
+    if(this.waitForSecondNumber)
+    {
+    this.currentNumber = v;
+    this.waitForSecondNumber = false;
+    }else{
+    this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
+    
+    }
+    
+    }
   constructor() { }
 
   ngOnInit(): void {
